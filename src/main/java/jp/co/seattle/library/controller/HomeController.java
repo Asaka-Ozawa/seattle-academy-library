8523da9db48879bd498c0388ceae276176c8f014
ozawaasaka <ozawaasaka@192.168.3.4>
2023-04-20 10:56:22 +0900

    削除ファイル修正

---
+++ src/main/java/jp/co/seattle/library/controller/HomeController.java
@@ -0,0 +1,40 @@
+package jp.co.seattle.library.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jp.co.seattle.library.dto.BookInfo;
+import jp.co.seattle.library.service.BooksService;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller // APIの入り口
+public class HomeController {
+	final static Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@Autowired
+	private BooksService booksService;
+
+	/**
+	 * Homeボタンからホーム画面に戻るページ
+	 * 
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public String transitionHome(Model model) {
+		//書籍の一覧情報を取得（タスク３）
+		List<BookInfo> bList = booksService.getBookList();
+		model.addAttribute("booklist", bList);
+		return "home";
+	}
+
+}
